fcd837a57abb9205ce06d2d8a0c5b9ed10d68089
Dheepak Krishnamurthy <me@kdheepak.com>
2019-07-29 15:35:44 -0600

    Update tests

--- test/test_pandoc.jl
+++ test/test_pandoc.jl
@@ -3,27 +3,34 @@
 @testset "test pandoc parser" begin
 
     @testset "docbook" begin
-
-        Pandoc.run_pandoc(joinpath(@__DIR__, "data", "docbook-reader.docbook"))
-
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "docbook-reader.docbook"))) == Pandoc.Document
     end
 
     @testset "creole" begin
-
-        Pandoc.run_pandoc(joinpath(@__DIR__, "data", "creole-reader.txt"))
-
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "creole-reader.txt"))) == Pandoc.Document
     end
 
     @testset "markdown" begin
-
-        Pandoc.run_pandoc(joinpath(@__DIR__, "data", "markdown-reader-more.txt"))
-
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "markdown-reader-more.txt"))) == Pandoc.Document
     end
 
     @testset "wiki" begin
-
-        Pandoc.run_pandoc(joinpath(@__DIR__, "data", "mediawiki-reader.wiki"))
-
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "mediawiki-reader.wiki"))) == Pandoc.Document
     end
 
+    @testset "ipynb" begin
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "simple.ipynb"))) == Pandoc.Document
+    end
+
+    @testset "latex" begin
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "latex-reader.latex"))) == Pandoc.Document
+    end
+
+    @testset "man" begin
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "man-reader.man"))) == Pandoc.Document
+    end
+
+    @testset "tables" begin
+        @test typeof(Pandoc.run_pandoc(joinpath(@__DIR__, "data", "pipe-tables.txt"))) == Pandoc.Document
+    end
 end
