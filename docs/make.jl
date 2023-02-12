1072315d9cf4a67daca22054de29e66ae070a200
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-12 01:00:30 -0500

    feat: Update Manifest ✨

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
-using Documenter, Pandoc, DocumenterMarkdown
+using Documenter, Pandoc
 
 cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "./src/index.md"); force = true, follow_symlinks = true)
 
-makedocs(; sitename = "Pandoc.jl documentation", format = Markdown())
+makedocs(; sitename = "Pandoc.jl documentation")
 
 deploydocs(; repo = "github.com/kdheepak/Pandoc.jl.git")
