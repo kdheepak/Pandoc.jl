dd563fc923ecb74871b5282eeb26c6a4eced9d56
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-12 00:53:11 -0500

    feat: Remove mkdocs ✨

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,7 @@
 using Documenter, Pandoc, DocumenterMarkdown
 
-cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "./src/index.md"), force=true, follow_symlinks=true)
+cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "./src/index.md"); force = true, follow_symlinks = true)
 
-makedocs(
-         sitename="Pandoc.jl documentation",
-         format = Markdown()
-        )
+makedocs(; sitename = "Pandoc.jl documentation", format = Markdown())
 
-deploydocs(
-    repo = "github.com/kdheepak/Pandoc.jl.git",
-    deps = Deps.pip(
-                   "mkdocs==0.17.5",
-                   "mkdocs-material==2.9.4",
-                   "python-markdown-math",
-                   "pygments",
-                   "pymdown-extensions",
-                   ),
-    make = () -> run(`mkdocs build`),
-    target = "site",
-)
-
+deploydocs(; repo = "github.com/kdheepak/Pandoc.jl.git")
