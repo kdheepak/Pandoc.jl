91aa87343437609cab86c4cc73d4578819dcd37f
Iagoba Apellaniz <iagoba.apellaniz@gmail.com>
2024-10-11 16:45:51 +0200

    Fix CI and tests removing Manifest.toml and allowing Pandoc API version in range (#24)
    * Delete Manifest.toml and untrack it
    
    * Update test for minor versions of pandoc api

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,15 +16,15 @@ Document = Pandoc.Document
 """,
     Document,
   )
-  @test doc.pandoc_api_version == v"1.23"
+  @test v"1.23" <= doc.pandoc_api_version < v"1.24"
 
   doc = Document(p"./data/example.md")
-  @test doc.pandoc_api_version == v"1.23"
+  @test v"1.23" <= doc.pandoc_api_version < v"1.24"
   @test length(doc.blocks) == 548
   JSON3.read(JSON3.write(doc), Dict)
 
   doc = Document(p"./data/example.txt")
-  @test doc.pandoc_api_version == v"1.23"
+  @test v"1.23" <= doc.pandoc_api_version < v"1.24"
   @test length(doc.blocks) == 1114
   JSON3.read(JSON3.write(doc), Dict)
 end
